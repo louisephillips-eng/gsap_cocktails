4a6bd3b8154a90d28d942e046f5138252c5c766a
louise phillips <miamoogbltd@yahoo.co.uk>
2026-05-30 16:47:21 +0100

    hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
+import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+//import { scrollTrigger } from "gsap/ScrollTrigger";
+import { useMediaQuery } from "react-responsive";
 
 
 const Hero = () => {
+    const videoRef = useRef();
+    const videoTimelineRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth:767 })
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words'});
 
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines'});
+
 
         heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
@@ -36,8 +44,32 @@ const Hero = () => {
             }
         })
             .to('.right-leaf', { y: 200 }, 0)
-            .to('.right-leaf', { y: -200}, 0)
-    }, [])
+            .to('.left-leaf', { y: -200}, 0)
+
+         const startValue = isMobile ? 'top 50%' : 'center 60%';
+         const endValue = isMobile ? '120% top' : 'bottom top';
+
+         //video animation timeline
+        //create the timeline with a default duration
+        const tl= gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
+
+    }, []);
+
+
     return (
         <>
         <section id="hero" className="noisy">
@@ -72,7 +104,18 @@ const Hero = () => {
                 </div>
             </div>
 
-        </section></>
+        </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                    />
+            </div>
+        </>
     )
 }
 export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         });
 
         navTween.fromTo('nav', { backgroundColor: 'transparent'},{
-            backgroundColor: '#0000050',
+            backgroundColor: '#00000050',
                 backgroundFilter: 'blur(10px',
                 duration: 1,
                 ease: 'power1.inOut'
